dbafa7a597aef57bf8d3a286bb4e4533ded7dfd8
nexguard <nexguard@163.com>
2019-02-05 17:17:08 +0800

    Site updated: 2019-02-05 17:17:08

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sun Jan 27 2019 14:31:06 GMT+0800 (Central Standard Time)
+// build time:Tue Feb 05 2019 17:17:01 GMT+0800 (Central Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
