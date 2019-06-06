fd3b85710e6970ea9f43b93e6596bbb53d168d6e
nexguard <nexguard@163.com>
2019-06-06 13:36:00 +0800

    Site updated: 2019-06-06 13:35:59

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun May 19 2019 04:06:43 GMT+0800 (Central Standard Time)
+// build time:Sun Jun 02 2019 16:49:48 GMT+0800 (Central Standard Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
