7149ee551c3d6e64c1b5379ed6c7eb731b267241
nexguard <nexguard@163.com>
2019-09-29 22:20:28 +0800

    Site updated: 2019-09-29 22:20:27

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 14 2019 11:26:52 GMT+0800 (Central Standard Time)
+// build time:Sun Sep 29 2019 22:20:20 GMT+0800 (Central Standard Time)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 14 2019 11:26:51 GMT+0800 (Central Standard Time)
+// build time:Sun Sep 29 2019 22:20:19 GMT+0800 (Central Standard Time)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
